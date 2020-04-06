178ea3ba725be5ba03d1a2db0784c1cf4370a488
Yongkun Li <indusdesign@163.com>
2020-04-06 07:59:39 -0400

    updated

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardModel : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    public Sprite[] districts;
+    public Sprite cardBack;
+    public int cardIndex; // districts[cardIndex]
+    public void ToggleFace(bool showface){
+        if (showface) {
+            // show card face
+            spriteRenderer.sprite = districts[cardIndex];
+        }else{
+            // show card back
+            spriteRenderer.sprite = cardBack;
+        }
+    }
+    
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DebugChangeCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugChangeCard : MonoBehaviour
+{
+    CardModel cardModel;
+    int cardIndex = 0;
+
+    public GameObject card;
+
+    private void Awake() {
+        cardModel = card.GetComponent<CardModel>();
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    private void OnGUI() {
+        if(GUI.Button(new Rect(10, 10, 100, 28), "Hit me")){
+            cardModel.cardIndex = cardIndex;
+            cardModel.ToggleFace(true);
+            cardIndex++;
+            if(cardIndex>cardModel.districts.Length-1){
+                cardIndex = 0;
+                cardModel.ToggleFace(false);
+            }
+        }
+    }
+}
